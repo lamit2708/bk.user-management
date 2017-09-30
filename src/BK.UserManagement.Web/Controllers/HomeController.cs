61fb1bc342c7486ec01d673a6e6dec7ca148bd7e
lamit2708 <lamit2708@gmail.com>
2017-10-01 02:46:29 +0700

    Update Connection String

--- src/BK.UserManagement.Web/Controllers/HomeController.cs
+++ src/BK.UserManagement.Web/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace BK.UserManagement.Web.Controllers
     {
         public string USER_ID { get; set; }
         public string USERNAME { get; set; }
+        //public string PASSWORD { get; set; }
         public string ACCOUNT_STATUS { get; set; }
         public string LOCK_DATE { get; set; }
         public string EXPIRE_DATE { get; set; }
@@ -20,10 +21,10 @@ namespace BK.UserManagement.Web.Controllers
 
     public class HomeController : Controller
     {
-        IConfiguration _iconfiguration;
-        public HomeController(IConfiguration iconfiguration)
+        private readonly IConfiguration config;
+        public HomeController(IConfiguration iconfig)
         {
-            _iconfiguration = iconfiguration;
+            config = iconfig;
         }
         public IActionResult Index()
         {
@@ -34,9 +35,11 @@ namespace BK.UserManagement.Web.Controllers
         {
             //_iconfiguration.GetSection("Data").GetSection("ConnectionString").Value;
             //_iconfiguration.GetValue<string>("ConnectionStrings:DefaultConnection")
+            //config.GetSection("ConnectionStrings")["DefaultConnection"]
+
             ViewData["Message"] = "Your application description page.";
             
-            using (var ole = new OracleConnection(_iconfiguration.GetSection("ConnectionStrings")["DefaultConnection"]))
+            using (var ole = new OracleConnection(config.GetConnectionString("DefaultConnection")))
             {
                 var users = ole.Query<DbUser>("SELECT * FROM dba_users");
                 return View(users);
