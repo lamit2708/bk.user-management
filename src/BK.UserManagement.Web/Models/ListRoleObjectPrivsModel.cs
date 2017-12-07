208c4e4e6532c637ef069815e7081e6f4f3d0b1f
thanhtamcctv <thanhtamcctv@gmail.com>
2017-12-07 21:13:56 +0700

    list role Object Privs

---
+++ src/BK.UserManagement.Web/Models/ListRoleObjectPrivsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BK.UserManagement.Web.Models
+{
+    public class ListRoleObjectPrivs
+    {
+        public string ROLE { get; set; }
+        public string OWNER { get; set; }
+        public string TABLE_NAME { get; set; }
+        public string COLUMN_NAME { get; set; }
+        public string PRIVILEGE { get; set; }
+        public string GRANTABLE { get; set; }
+    }
+}
