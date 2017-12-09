cc395e43935860da013744a6c60ef8fe0c75661a
phucnguyen3105@gmail.com <npk312224>
2017-12-09 17:47:17 +0700

    update sys priv

--- src/BK.UserManagement.Web/Controllers/PrivilegeController.cs
+++ src/BK.UserManagement.Web/Controllers/PrivilegeController.cs
@@ -29,31 +29,69 @@ namespace BK.UserManagement.Web.Controllers
         [HttpGet]
         public IActionResult GrantSysPrivs(string id)
         {
-
-
-
             using (var ole = new OracleConnection(config.GetConnectionString("DefaultConnection")))
             {
 
-                var listPriv = ole.Query($"select * from SESSION_PRIVS");
-                var sl = listPriv.Select(x =>
-                                    new SelectListItem()
-                                    {
-                                        Text = x.PRIVILEGE.ToString(),
-                                        Value = x.PRIVILEGE.ToString()
-                                    });
-                ViewBag.ListPriv = sl.ToList();
+                //var listPriv = ole.Query($"select * from SESSION_PRIVS");
+                //var sl = listPriv.Select(x =>
+                //                    new SelectListItem()
+                //                    {
+                //                        Text = x.PRIVILEGE.ToString(),
+                //                        Value = x.PRIVILEGE.ToString()
+                //                    });
+                //ViewBag.ListPriv = sl.ToList();
 
+
+                 ViewBag.ListPriv = new SelectList(new[] { "CREATE PROFILE", "ALTER PROFILE", "DROP PROFILE", "CREATE ROLE", "ALTER ANY ROLE", "DROP ANY ROLE", "GRANT ANY ROLE", "CREATE SESSION"
+                 , "CREATE ANY TABLE", "ALTER ANY TABLE", "DROP ANY TABLE", "SELECT ANY TABLE", "DELETE ANY TABLE", "INSERT ANY TABLE", "UPDATE ANY TABLE", "CREATE TABLE", "CREATE USER", "ALTER USER", "DROP USER" }, "");
                 ViewBag.ListAdmin = new SelectList(new[] { "YES", "NO"}, "YES");
                 ViewBag.ListType = new SelectList(new[] { "GRANT", "REVOKE" }, "GRANT");
-
+                ViewBag.Username = id;
                 var listSysPriv = ole.Query<SysPrivsModel>($"select * from sys.DBA_SYS_PRIVS WHERE GRANTEE = '{id}'");
                 return View(listSysPriv);
             }
 
-
-               
         }
+
+
+        [HttpPost]
+        public IActionResult GrantSysPrivs(string _username,string ListPriv,string ListAdmin,string ListType)
+        {
+            using (var ole = new OracleConnection(config.GetConnectionString("DefaultConnection")))
+            {
+                if (ListType == "GRANT")
+                {
+
+                    var iExist = ole.Query($@"SELECT * FROM DBA_SYS_PRIVS WHERE GRANTEE = '{_username}' AND PRIVILEGE LIKE '{ListPriv}%'").Count();
+                    if (iExist > 0)
+                    {
+                        ole.Query<string>($@"REVOKE {ListPriv} FROM {_username}");
+                    }
+
+                    
+                    //ole.Query<string>($@"REVOKE {ListPriv} FROM {_username}");
+
+                    if (ListAdmin == "YES")
+                    {
+                        ole.Query<string>($@"{ListType} {ListPriv} TO {_username} WITH ADMIN OPTION");
+                    }
+                    else
+                    {
+                        ole.Query<string>($@"{ListType} {ListPriv} TO {_username}");
+                    }
+                    
+                }
+                else if(ListType == "REVOKE")
+                {
+                    ole.Query<string>($@"{ListType} {ListPriv} FROM {_username}");
+                }
+                
+
+                return RedirectToAction("GrantSysPrivs",new { id = _username });
+            }
+
+        }
+
 
     }
 }
