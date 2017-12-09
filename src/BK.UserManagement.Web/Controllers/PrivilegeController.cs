91b39600c2fae8d0eb1be1ebf1083144acd119e3
phucnguyen3105@gmail.com <npk312224>
2017-12-09 12:53:16 +0700

    update priv

--- src/BK.UserManagement.Web/Controllers/PrivilegeController.cs
+++ src/BK.UserManagement.Web/Controllers/PrivilegeController.cs
@@ -3,19 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using BK.UserManagement.Web.Models;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BK.UserManagement.Web.Controllers
 {
     public class PrivilegeController : Controller
     {
+
+        private readonly IConfiguration config;
+        private string connString;
+
+        public PrivilegeController(IConfiguration iconfig)
+        {
+            config = iconfig;
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult GrantSysPrivs()
+        [HttpGet]
+        public IActionResult GrantSysPrivs(string id)
         {
-            return View();
+
+
+
+            using (var ole = new OracleConnection(config.GetConnectionString("DefaultConnection")))
+            {
+
+                var listPriv = ole.Query($"select * from SESSION_PRIVS");
+                var sl = listPriv.Select(x =>
+                                    new SelectListItem()
+                                    {
+                                        Text = x.PRIVILEGE.ToString(),
+                                        Value = x.PRIVILEGE.ToString()
+                                    });
+                ViewBag.ListPriv = sl.ToList();
+
+                ViewBag.ListAdmin = new SelectList(new[] { "YES", "NO"}, "YES");
+                ViewBag.ListType = new SelectList(new[] { "GRANT", "REVOKE" }, "GRANT");
+
+                var listSysPriv = ole.Query<SysPrivsModel>($"select * from sys.DBA_SYS_PRIVS WHERE GRANTEE = '{id}'");
+                return View(listSysPriv);
+            }
+
+
+               
         }
 
     }
